5463044a96aa39f0f8cc6233162c0ff29dd70014
Mesrobian <1wamesrobian@seattleschools.org>
2023-10-09 09:03:42 -0700

    submiting things

---
+++ OwnObjectClass.java
@@ -0,0 +1,50 @@
+/* Class:OwnObjectClass
+By: Willow Mesrobian
+Created for AP CS 10/09/2023
+*/
+public class OwnObjectClass {
+      public static void main (String[] args) {
+      TellTheTime(); 
+      SetTheTime();
+      SetTimer();
+      StopTimer();
+      CurrentMinutes();
+     }
+     
+     public static void TellTheTime() {
+     //To be able to know the current time
+        System.out.println("It is 9:01");
+     
+     
+     }
+     
+     
+     public static void SetTheTime() {
+     //To be able to change the time it says to the time it actually currently is
+     
+     
+     
+     
+     }
+     
+     public static void SetTimer() {
+     //To set the clock to have a timer that will ring at a certain time set
+     
+     
+     
+     
+    }
+    
+    public static void StopTimer() {
+    //To stop the timer once the message is properly recieved
+    
+    
+    
+    }
+    
+    public static void CurrentMinutes() {
+    //To tell the current time
+    
+    
+    }
+}
---
+++ PlayingWithVariables.java
@@ -0,0 +1,17 @@
+public class PlayingWithVariables {
+     public static void main(String[] args) {
+        int myInteger;
+        double myDouble;
+        double mySum;
+        
+        myInteger = 3;
+        myDouble = 3;
+        mySum = myInteger + myDouble;
+        
+        System.out.println("myInteger = " + myInteger);
+        System.out.println("myDouble = " +myDouble);
+        System.out.println("mySum = " + myDouble + myInteger); 
+        System.out.println("myDouble + myInt = " + myDouble + myInteger);
+        
+        }
+}
